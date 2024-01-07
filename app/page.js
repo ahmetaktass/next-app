c301f404b50a8b08fc7563864a8b8c1d0b8713b0
Ahmet Aktaş <mail.ahmetaktas@gmail.com>
2024-01-08 01:00:34 +0300

    feat(redux install): #1

--- app/page.js
+++ app/page.js
@@ -1,7 +1,24 @@
+"use client";
+import React from "react";
+import { useAppDispatch, useAppSelector } from "../lib/hooks";
+import { decrement, increment } from "../lib/features/counterSlice";
+
 export default function Home() {
+  const dispatch = useAppDispatch();
+  const counter = useAppSelector((state) => state.counter);
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
   return (
     <div className="h-[90vh]">
-      <h1> Anasayfa</h1>
+      <p>Counter: {counter}</p>
+      <button onClick={handleIncrement}>Arttır</button>
+      <button onClick={handleDecrement}>Azalt</button>
     </div>
   );
 }
