ccbcf76bce31a6d28b3fc7bc8c51f7a50502ec29
Ahmet Aktaş <mail.ahmetaktas@gmail.com>
2024-01-08 20:32:13 +0300

    feat(search user add): #3 search user component added

--- app/users/page.js
+++ app/users/page.js
@@ -8,12 +8,16 @@ import Grid from "@mui/material/Grid";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteUserDialog from "./DeleteUserDialog";
+import { Button, Container, TextField } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
 
 const Users = () => {
   const dispatch = useDispatch();
   const userData = useSelector((state) => state.users.data);
   const [openDialog, setOpenDialog] = React.useState(false);
   const [deleteUserId, setDeleteUserId] = React.useState(null);
+  const [searchTerm, setSearchTerm] = React.useState("");
 
   useEffect(() => {
     dispatch(fetchUsers());
@@ -36,63 +40,94 @@ const Users = () => {
       console.error("Silme işlemi sırasında hata oluştu:", error);
     }
   };
+  const filteredUsers = userData?.data.filter((user) => {
+    const fullName = `${user.firstName} ${user.lastName}`.toLowerCase();
+    return fullName.includes(searchTerm.toLowerCase());
+  });
 
   return (
-    <Grid container spacing={2}>
-      {userData &&
-        userData?.data.map((user) => (
-          <Grid item xs={12} sm={6} lg={3} key={user.id}>
-            <div className="flex items-center group bg-gray-100 p-5 rounded-md m-2 relative cursor-pointer">
-              <Image
-                src={user.picture}
-                alt={`${user.firstName} ${user.lastName}`}
-                width={80}
-                height={80}
-                className="rounded-full"
-              />
-              <div className="ml-4">
-                <h2 className="text-2xl font-bold">
-                  {user.firstName} {user.lastName}
-                </h2>
-                <p className="text-gray-500">{user.title}</p>
-                <DeleteIcon
-                  sx={{
-                    color: "red",
-                    width: 30,
-                    height: 30,
-                    cursor: "pointer",
-                  }}
-                  onClick={() => handleDelete(user.id)}
-                  className="absolute top-2 right-2 hidden group-hover:block "
-                >
-                  Sil
-                </DeleteIcon>
-                <EditIcon
-                  sx={{
-                    color: "blue",
-                    width: 30,
-                    height: 30,
-                    cursor: "pointer",
-                  }}
-                  className="absolute top-10 right-2 hidden group-hover:block "
-                >
-                  Düzenle
-                </EditIcon>
+    <>
+      <div className="flex  gap-x-5 justify-center items-center w-1/2 mx-auto">
+        <TextField
+          className=" w-3/4 my-3"
+          label="Kullanıcı Ara..."
+          variant="outlined"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          fullWidth
+          InputProps={{
+            endAdornment: <SearchIcon color="action" sx={{ marginRight: 1 }} />,
+          }}
+        />
+        <Button
+          variant="contained"
+          style={{
+            backgroundColor: "#3468C0",
+            color: "success",
+            height: "50px",
+          }}
+          endIcon={<PersonAddIcon />}
+        >
+          Ekle
+        </Button>
+      </div>
+
+      <Grid container spacing={2}>
+        {filteredUsers &&
+          filteredUsers.map((user) => (
+            <Grid item xs={12} sm={6} lg={3} key={user.id}>
+              <div className="flex items-center group bg-gray-100 p-5 rounded-md m-2 relative cursor-pointer">
+                <Image
+                  src={user.picture}
+                  alt={`${user.firstName} ${user.lastName}`}
+                  width={80}
+                  height={80}
+                  className="rounded-full"
+                />
+                <div className="ml-4">
+                  <h2 className="text-2xl font-bold">
+                    {user.firstName} {user.lastName}
+                  </h2>
+                  <p className="text-gray-500">{user.title}</p>
+                  <DeleteIcon
+                    sx={{
+                      color: "red",
+                      width: 30,
+                      height: 30,
+                      cursor: "pointer",
+                    }}
+                    onClick={() => handleDelete(user.id)}
+                    className="absolute top-2 right-2 hidden group-hover:block "
+                  >
+                    Sil
+                  </DeleteIcon>
+                  <EditIcon
+                    sx={{
+                      color: "blue",
+                      width: 30,
+                      height: 30,
+                      cursor: "pointer",
+                    }}
+                    className="absolute top-10 right-2 hidden group-hover:block "
+                  >
+                    Düzenle
+                  </EditIcon>
+                </div>
               </div>
-            </div>
-          </Grid>
-        ))}
-      <DeleteUserDialog
-        open={openDialog}
-        onClose={handleDialogClose}
-        onConfirm={handleConfirmDelete}
-        name={
-          userData?.data.find((user) => user.id === deleteUserId)?.firstName +
-          " " +
-          userData?.data.find((user) => user.id === deleteUserId)?.lastName
-        }
-      />
-    </Grid>
+            </Grid>
+          ))}
+        <DeleteUserDialog
+          open={openDialog}
+          onClose={handleDialogClose}
+          onConfirm={handleConfirmDelete}
+          name={
+            userData?.data.find((user) => user.id === deleteUserId)?.firstName +
+            " " +
+            userData?.data.find((user) => user.id === deleteUserId)?.lastName
+          }
+        />
+      </Grid>
+    </>
   );
 };
 
